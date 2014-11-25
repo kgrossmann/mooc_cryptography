1e9ce0fcef08dc69fb95aba74d71cd387ad1705a
Klemens Grosmann <klemensgrossmann@t-online.de>
2014-11-25 17:43:00 +0100

    lddl

---
+++ src/week1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : week1.cpp
+// Author      : Klemens Gro?mann
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "MOOC Cryptography" << endl; // prints MOOC Cryptography
+	return 0;
+}
