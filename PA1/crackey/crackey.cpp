e84695fb48cc9826871e0f4687d5e47483378edc
Klemens Grosmann <klemensgrossmann@t-online.de>
2014-11-27 17:25:31 +0100

    Tidied up the type declarations

--- PA1/crackey/crackey.cpp
+++ PA1/crackey/crackey.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : crack_key_len.cpp
-// Author      : Klemens Gross	mann
-// Version     :
+// Author      : Klemens Grossmann
+// Version     : V 2.0
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
@@ -16,37 +16,39 @@
 //
 using namespace std;
 
+#define KEY_CHAR_RANGE_LOWER_BOUND 0
+#define KEY_CHAR_RANGE_UPPER_BOUND 255
 #define MAX_KEY_LENGTH 512
 #define ENGLISH_LETTER_FREQUENCY 0.065
 #define STREAM_TOLERANCE_AGAINST_ENGLISH_LETTER_FREQUENCY 0.10
 #define CFILE "ctext.txt"
 #define PFILE "ptext.txt"
 
-typedef std::map<int,int> CHARLIST;
+typedef std::map<unsigned int,unsigned int> CHARLIST;
 static char msgbuf[1024];
 //
-std::map<unsigned int,int>* CreateInitializeHistogram() {
-	std::map<unsigned int,int>* histogram = new std::map<unsigned int,int>;
-	for(unsigned int i=0;i<=255;i++) {
-	   histogram->insert(pair<unsigned int,int>(i,0));
+std::map<unsigned int,unsigned int>* CreateInitializeHistogram() {
+	std::map<unsigned int,unsigned int>* histogram = new std::map<unsigned int,unsigned int>;
+	for(unsigned int i=KEY_CHAR_RANGE_LOWER_BOUND;i<=KEY_CHAR_RANGE_UPPER_BOUND;i++) {
+	   histogram->insert(pair<unsigned int,unsigned int>(i,0));
 	}
 	return histogram;
 }
 //
-std::map<int,CHARLIST>* CreateInitializeKeyCharacters(unsigned int keylen) {
-	std::map<int,CHARLIST>* keyscharacters = new std::map<int,CHARLIST>;
+std::map<unsigned int,CHARLIST>* CreateInitializeKeyCharacters(unsigned int keylen) {
+	std::map<unsigned int,CHARLIST>* keyscharacters = new std::map<unsigned int,CHARLIST>;
 	for(int i=1;i<=keylen;i++) {
 		CHARLIST charlist;
-		keyscharacters->insert(pair<int,CHARLIST>(i,charlist));
+		keyscharacters->insert(pair<unsigned int,CHARLIST>(i,charlist));
 	}
 	return keyscharacters;
 }
 //
-int SumUpPossibleKeys(std::map<int,CHARLIST>* keyscharacters)  {
+int SumUpPossibleKeys(std::map<unsigned int,CHARLIST>* keyscharacters)  {
 	int iPossibleKeys = 0;
 	if(keyscharacters->size()>0) {
 		iPossibleKeys = 1;
-		for (std::map<int,CHARLIST>::iterator it=keyscharacters->begin();it!=keyscharacters->end();it++) {
+		for (std::map<unsigned int,CHARLIST>::iterator it=keyscharacters->begin();it!=keyscharacters->end();it++) {
 			CHARLIST keylist = it->second;
 			iPossibleKeys *= keylist.size();
 		}
@@ -54,24 +56,27 @@ int SumUpPossibleKeys(std::map<int,CHARLIST>* keyscharacters)  {
 	return iPossibleKeys;
 }
 //
-std::map<int,int>* CreateInitializePromisingKeyLength(unsigned int maxNumberOfKeys) {
-	std::map<int,int>* promisingkeys = new std::map<int,int>;
+std::map<unsigned int,unsigned int>* CreateInitializePromisingKeyLength(unsigned int maxNumberOfKeys) {
+	std::map<unsigned int,unsigned int>* promisingkeys = new std::map<unsigned int,unsigned int>;
 	for(unsigned int k=1;k<=maxNumberOfKeys;k++) {
-		promisingkeys->insert(pair<int,int>(k,0));
+		promisingkeys->insert(pair<unsigned int,unsigned int>(k,0));
 	}
 	return promisingkeys;
 }
 //
-float CalculateScoreFromHistogram(std::map<unsigned int,int>* histogram, unsigned int letters_total) {
+float CalculateScoreFromHistogram(std::map<unsigned int,unsigned int>* histogram, unsigned int letters_total) {
    float fdistributinScore = 0.0;
-   for (std::map<unsigned int,int>::iterator it=histogram->begin();it!=histogram->end();it++) {
+   for (std::map<unsigned int,unsigned int>::iterator it=histogram->begin();it!=histogram->end();it++) {
 	   if (it->second>0) {
+		   //check the plaintext character to be in the valid range
 		   if (it->first>=32 && it->first<=127
 			  && (it->first != '~' && /*it->first != '{' && it->first != '}' &&*/ it->first != '#' && it->first != '`') //excluse some punctuations
 			  && (isupper(it->first) || islower(it->first) || ispunct(it->first) || isspace(it->first))) {
 			   float real_probabilty = it->second/(float)letters_total;
 			   fdistributinScore += real_probabilty*real_probabilty;
 		   }  else {
+			   //an allowed plainttext character was created by decrypting the current trial key or trial key character
+			   //this result in a score of 0.0, in order to have those invalid trials keys weeded out
 			   return 0.0;
 		   }
 	   }
@@ -79,7 +84,7 @@ float CalculateScoreFromHistogram(std::map<unsigned int,int>* histogram, unsigne
    return (fdistributinScore);
 }
 //
-void DecryptByKeyAndPrint(std::map<int,int>* key) {
+void DecryptByKeyAndPrint(std::map<unsigned int,unsigned int>* key) {
     FILE* cfile  = fopen(CFILE,"r");
     FILE* pfile  = fopen(PFILE,"w");
     if((pfile==NULL) || (cfile==NULL)) {
@@ -87,9 +92,9 @@ void DecryptByKeyAndPrint(std::map<int,int>* key) {
     	return;
     }
     unsigned int letters_total = 0;
-    std::map<unsigned int,int>* histogram = CreateInitializeHistogram();
+    std::map<unsigned int,unsigned int>* histogram = CreateInitializeHistogram();
     std::cout << "-----------------------------------------------------------------\n";
-   	sprintf(msgbuf,"%s","Ciphertext decrypted by key  ");
+   	sprintf(msgbuf,"%s","Ciphertext decrypted by key ");
    	fprintf(pfile,"%s",msgbuf);
    	std::cout << msgbuf;
     for(int i=1;i<=key->size();i++)	{
@@ -132,11 +137,11 @@ void DecryptByKeyAndPrint(std::map<int,int>* key) {
     fclose(cfile);
 }
 //
-unsigned int IterateThroughPromisingKeysAndShowStats(std::map<int,int>* promisingkeys) {
+unsigned int IterateThroughPromisingKeysAndShowStats(std::map<unsigned int,unsigned int>* promisingkeys) {
 	unsigned int iBestKeyIndex=0;
 	float fbestScore = 0.0;
 	std::cout << "-----------------------------------------------------------------\n";
-	for (std::map<int,int>::iterator it=promisingkeys->begin();it!=promisingkeys->end();it++) {
+	for (std::map<unsigned int,unsigned int>::iterator it=promisingkeys->begin();it!=promisingkeys->end();it++) {
 		if(it->second>0) {
 			iBestKeyIndex = it->first;
 			std::cout << "promising keylen: " << it->first << ", number of possible keys: " << it->second << "\n";
@@ -145,14 +150,14 @@ unsigned int IterateThroughPromisingKeysAndShowStats(std::map<int,int>* promisin
 	return iBestKeyIndex;
 }
 //
-void IterateThroughKeysAndDecrypt(std::map<int,CHARLIST>* keyscharacters, unsigned int keylen,std::map<int,int>* characters_so_far) {
+void IterateThroughKeysAndDecrypt(std::map<unsigned int,CHARLIST>* keyscharacters, unsigned int keylen,std::map<unsigned int,unsigned int>* characters_so_far) {
 	unsigned int number_of_characters_so_far = characters_so_far->size();
 	if(characters_so_far->size()==keylen) {
 		DecryptByKeyAndPrint(characters_so_far);
 		return;
 	} else {
 		CHARLIST charlist = (*keyscharacters)[number_of_characters_so_far+1];
-		for (std::map<int,int>::iterator it=charlist.begin();it!=charlist.end();it++) {
+		for (std::map<unsigned int,unsigned int>::iterator it=charlist.begin();it!=charlist.end();it++) {
 			characters_so_far->insert(pair<int,int>(characters_so_far->size()+1,it->first));
 			IterateThroughKeysAndDecrypt(keyscharacters,keylen,characters_so_far);
 			characters_so_far->erase(characters_so_far->size());
@@ -160,12 +165,12 @@ void IterateThroughKeysAndDecrypt(std::map<int,CHARLIST>* keyscharacters, unsign
 	}
 }
 //
-void IterateThroughPromisingKeysAndDecrypt(std::map<int,int>* promisingkeys,std::map<int,CHARLIST>** keyscharacters) {
-	for (std::map<int,int>::iterator it=promisingkeys->begin();it!=promisingkeys->end();it++) {
+void IterateThroughPromisingKeysAndDecrypt(std::map<unsigned int,unsigned int>* promisingkeys,std::map<unsigned int,CHARLIST>** keyscharacters) {
+	for (std::map<unsigned int,unsigned int>::iterator it=promisingkeys->begin();it!=promisingkeys->end();it++) {
 		if(it->second>0) {
-			int keylen = it->first;
-			std::map<int,CHARLIST>* charactersforkey = keyscharacters[keylen];
-			std::map<int,int>* characters_so_far = new std::map<int,int>;
+			unsigned int keylen = it->first;
+			std::map<unsigned int,CHARLIST>* charactersforkey = keyscharacters[keylen];
+			std::map<unsigned int,unsigned int>* characters_so_far = new std::map<unsigned int,unsigned int>;
 			IterateThroughKeysAndDecrypt(charactersforkey,keylen,characters_so_far);
 			delete characters_so_far;
 		}
@@ -187,8 +192,8 @@ int main() {
 	unsigned int textlen = DetermineTextLegnth();
 	std::cout << "text length : " << textlen << "\n";
 	//
-	std::map<int,int>* promisingkeys = CreateInitializePromisingKeyLength(textlen);
-	std::map<int,CHARLIST>* keyscharacters[MAX_KEY_LENGTH+1];
+	std::map<unsigned int,unsigned int>* promisingkeys = CreateInitializePromisingKeyLength(textlen);
+	std::map<unsigned int,CHARLIST>* keyscharacters[MAX_KEY_LENGTH+1];
 	//
 	for(int keylength=1;keylength<=textlen;keylength++)
 	{
@@ -199,10 +204,10 @@ int main() {
 			//each iteration here goes through a i-stream of the key
 			//characters in this stream are all decrypted by the same character in the key
 			bool bAtLeastOneViableKeyFound = false;
-			for(unsigned int keychar=0;keychar<=255;keychar++) {
+			for(unsigned int keychar=KEY_CHAR_RANGE_LOWER_BOUND;keychar<=KEY_CHAR_RANGE_UPPER_BOUND;keychar++) {
 				FILE* fcIn = fopen(CFILE,"r");
 				unsigned int letters_total = 0;
-				std::map<unsigned int,int>* histogram = CreateInitializeHistogram();
+				std::map<unsigned int,unsigned int>* histogram = CreateInitializeHistogram();
 				int i=0;
 				unsigned char ch;
 				while (fscanf(fcIn,"%2hhX",&ch)!=EOF) {
