47e8535ea48e27dc3c98d0d8f494634ddfa50315
Klemens Grosmann <klemensgrossmann@t-online.de>
2014-12-04 08:43:15 +0100

    PA4 stripped down

--- PA4/src/cbcmacattack.cpp
+++ PA4/src/cbcmacattack.cpp
@@ -1,28 +1,25 @@
 #include "oracle.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <iostream>
-#include <cstdlib>
 #include <cstring>
 using namespace std;
 
 #define MAX_LENGTH 512
 #define BLOCK_LENGTH 16
-static string attack_msg = "I, the server, hereby agree that I will pay $100 to this student";
-//
+
 void PrintOutTag(unsigned char* tag) {
-	cout << "Forged tag for messsage '" << attack_msg.c_str() << "': " <<  endl;
+	cout << "Forged tag: " <<  endl;
 	for(unsigned int i=0;i<BLOCK_LENGTH;i++) {
 		printf("%2X",tag[i]);
 	}
-	printf("\n");
+	cout << endl;
 }
-//
+
 void LetOracleTagMessage(unsigned char* message, unsigned int msglen, unsigned char* tag) {
 	Mac(message,(int)msglen,tag);
 }
-//
+
 void LetOracleVerifyTagAndMessage(unsigned char* message, unsigned int msglen, unsigned char* tag) {
 	if (Vrfy(message,(int)msglen, tag)) {
 		printf("Message verified successfully!\n");
@@ -30,37 +27,34 @@ void LetOracleVerifyTagAndMessage(unsigned char* message, unsigned int msglen, u
 		printf("Message verficiation failed.\n");
 	}
 }
-//
+
 int main(int argc, char *argv[]) {
 	cout << "MOOC Cryptography - PA4" << endl;
+	string attack_msg = "I, the server, hereby agree that I will pay $100 to this student";
 	unsigned char message[MAX_LENGTH];
-	unsigned char taggedmessage[MAX_LENGTH];
 	unsigned char tag[BLOCK_LENGTH];
 	unsigned int  msglen = attack_msg.size();
+	memset(message,0,MAX_LENGTH);
 
 	Oracle_Connect();
 
-	memset(message,0,MAX_LENGTH);
-	memset(taggedmessage,0,MAX_LENGTH);
-	for(unsigned int i=0;i<msglen;i++) 	message[i] = attack_msg[i];
-	//send the first two blocks to the MAC oracle and retrieve the intermediate tag in the
-	//CBC-MAC chain
-	for(unsigned int i=0;i<2*BLOCK_LENGTH;i++) taggedmessage[i] = message[i];
-	LetOracleTagMessage(taggedmessage,2*BLOCK_LENGTH,tag);
-	//the XOR of the thirst message block with this intermediate tag gives the first block
-	//of the next 2-block message to send to the MAC oracle for retrieving the final forged tag
-	for(unsigned int i=0;i<BLOCK_LENGTH;i++) taggedmessage[i] = tag[i]^message[2*BLOCK_LENGTH+i];
-	//the second block of the next 2-block message to send is the 4th block of the text
-	for(unsigned int i=0;i<BLOCK_LENGTH;i++) taggedmessage[BLOCK_LENGTH+i] = message[3*BLOCK_LENGTH+i];
-	//sending this 2-block message, with the first input message being
-	//the input to the third block cipher in the MAC chain, gives the
-	//forged tag for the attack message
-	LetOracleTagMessage(taggedmessage,2*BLOCK_LENGTH,tag);
+	//send the first two blocks to the MAC oracle and retrieve the intermediate tag (for 2 blocks)
+	//in the 4-blocks CBC-MAC chain
+	for(unsigned int i=0;i<2*BLOCK_LENGTH;i++) message[i] = attack_msg[i];
+	LetOracleTagMessage(message,2*BLOCK_LENGTH,tag);
+	//the XOR of the third message block with the intermediate tag gives the first block
+	//of the next 2-block message. Sending tht next 2-block messaged to the MAC oracle yields
+	//the final forged tag
+	for(unsigned int i=0;i<BLOCK_LENGTH;i++) message[i] = tag[i]^attack_msg[2*BLOCK_LENGTH+i];
+	//the second block of the next 2-block message is the 4th block of the text:
+	for(unsigned int i=0;i<BLOCK_LENGTH;i++) message[BLOCK_LENGTH+i] = attack_msg[3*BLOCK_LENGTH+i];
+	//sending this 2-block message to the oracle, we obtain the forged tag
+	LetOracleTagMessage(message,2*BLOCK_LENGTH,tag);
 	//sending that tag along with the attack message should becverified with success
-	LetOracleVerifyTagAndMessage(message,msglen,tag);
-	//
+	LetOracleVerifyTagAndMessage((unsigned char*)attack_msg.c_str(),msglen,tag);
+
 	Oracle_Disconnect();
-	//
+
 	PrintOutTag(tag);
 	return 0;
 }
