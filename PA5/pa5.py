3c7bc873aecc260f3f6892b017a66ccfd6624c19
Klemens Grosmann <klemensgrossmann@t-online.de>
2014-12-21 16:37:53 +0100

    RSA forgery attack solved(PA5)

--- PA5/pa5.py
+++ PA5/pa5.py
@@ -4,13 +4,16 @@ from oracle import *
 from helper import *
 from decimal import Decimal
 from helper import modinv
-#
+# Launches a forgery on a padded-message RSA signature scheme, based on a plain RSA signature
+# This is the message to be forded on the oracle
 msg = "Crypto is hard --- even schemes that look complex can be broken"     
 #
-# RSA key (decimal)
-N = 119077393994976313358209514872004186781083638474007212865571534799455802984783764695504518716476645854434703350542987348935664430222174597252144205891641172082602942313168180100366024600206994820541840725743590501646516068078269875871068596540116450747659687492528762004294694507524718065820838211568885027869
-mu = 2**(64*8)+1
+# public RSA key (decimal)
 e = 65537
+N = 119077393994976313358209514872004186781083638474007212865571534799455802984783764695504518716476645854434703350542987348935664430222174597252144205891641172082602942313168180100366024600206994820541840725743590501646516068078269875871068596540116450747659687492528762004294694507524718065820838211568885027869
+# the padding scheme P(m) is defined as P(m) = w*m + r, where w is the shiftin coefficient, and r is a padding coefficient
+# Here, with the padded message M = 0x00 m 0x00 m , r is zero and w = 2**(64*8)+1
+mu = 2**(64*8)+1
 #
 def CalculateMessageValue(message):
     val = 0    
@@ -24,10 +27,6 @@ def PrintSomethingOutHex(msg,val):
    print(msg + str(":"))
    print(hex(val))
 #
-def PrintSomethingOutDec(msg,val):
-   print(msg + str(":"))
-   print(val)
-#
 print(msg)
 m =  CalculateMessageValue(msg)
 m1 = 2
@@ -39,34 +38,30 @@ PrintSomethingOutHex("m2",m2)
 assert(N>mu*m1)
 assert(N>mu*m2)
 assert(N>m1*m2)
-#
+# connect to python oracle
 Oracle_Connect()
-#sigmaok = Sign(m)
-#assert(sigmaok>=0 and sigmaok<N)
-#PrintSomethingOutHex("sigmaok",sigmaok)
 sigma1 = Sign(m1)
 assert(sigma1>=0 and sigma1<N)
-PrintSomethingOutHex("sigma1",sigma1)
 sigma2 = Sign(m2)
 assert(sigma2>=0 and sigma2<N)
-PrintSomethingOutHex("sigma2",sigma2)
 sigma_mu = Sign(0x01)
 assert(pow(sigma_mu,e,N)==mu)
-PrintSomethingOutHex("sigma_mu",sigma_mu)
-#
+# After deriving a linear equatin for the e-th power of the signature on
+# m (sigma(m)^e = sigma(m1)^e*sigma(m2)^e/sigma(1)^e) = mu*m1*m2), we verfiy
+# its validity here:
 sigma1_e = pow(sigma1,e,N)
 sigma2_e = pow(sigma2,e,N)
 sigma_mu_e = pow(sigma_mu,e,N)
-sigma_e = sigma1_e*sigma2_e//sigma_mu_e
-PrintSomethingOutHex("sigma_e",sigma_e)
-assert(sigma_e==mu*m1*m2)
-#if the above is true, can we not derive the following?
+assert(sigma1_e*sigma2_e//sigma_mu_e==mu*m1*m2)
+# if the above is true, we simply can take the e-th root in order to
+# obtain the signature on m;
+# the e-th root is then [sigma(m1)*sigma(m2)/sigma(1) mod N];
+# however, most importantly, note that modolu division by 'b' is done by
+# multiplying with the modolu inverse of b, that is b^-1 = modinv(b,N)
 sigma_mu_inv = modinv(sigma_mu,N)
 sigma =  (sigma1*sigma2*sigma_mu_inv)%N
 assert(sigma<N)
 PrintSomethingOutHex("sigma",sigma)   
-#PrintSomethingOutDec("sigmaok/sigma",Decimal(sigmaok/sigma))
-#
 if Verify(m,sigma):
    print("Well done!")
 else:     
