91dd4191b861da75c9ab9e9ebdee5c38923abc18
Klemens Grosmann <klemensgrossmann@t-online.de>
2014-12-21 15:00:13 +0100

    for me promising solution to pa5, but calculating the equation out fails to give the correct result.

--- PA5/pa5.py
+++ PA5/pa5.py
@@ -36,44 +36,37 @@ assert(m1*m2==m)
 PrintSomethingOutHex("m",m)
 PrintSomethingOutHex("m1",m1)
 PrintSomethingOutHex("m2",m2)
-assert(N>mu)
 assert(N>mu*m1)
 assert(N>mu*m2)
 assert(N>m1*m2)
 #
 Oracle_Connect()
 sigmaok = Sign(m)
+assert(sigmaok>=0 and sigmaok<N)
 PrintSomethingOutHex("sigmaok",sigmaok)
-#
 sigma1 = Sign(m1)
+assert(sigma1>=0 and sigma1<N)
 PrintSomethingOutHex("sigma1",sigma1)
-assert(sigma1>=0)
-assert(sigma1<N)
 sigma2 = Sign(m2)
+assert(sigma2>=0 and sigma2<N)
 PrintSomethingOutHex("sigma2",sigma2)
-assert(sigma2>=0)
-assert(sigma2<N)
-mu_inv = modinv(mu,N)
-PrintSomethingOutHex("mu_inv",mu_inv)
 sigma_mu = Sign(0x01)
-assert(sigma_mu>=0)
-assert(sigma_mu<N)   
 assert(pow(sigma_mu,e,N)==mu)
 PrintSomethingOutHex("sigma_mu",sigma_mu)
-if (m1*m2==m):                        
-   sigma1_e = pow(sigma1,e,N)
-   sigma2_e = pow(sigma2,e,N)
-   sigma_mu_e = pow(sigma_mu,e,N)
-   assert(sigma1_e*sigma2_e//sigma_mu_e == mu*m1*m2)
-   assert(pow(sigma1,e,N)*pow(sigma2,e,N)//pow(sigma_mu,e,N) == mu*m1*m2)
-   sigma = (sigma1*sigma2//sigma_mu)%N      
-   PrintSomethingOutHex("sigma",sigma)   
-   if Verify(m,sigma):
-      print("Well done!")
-   else:     
-      print("Try Again!")
 #
+sigma1_e = pow(sigma1,e,N)
+sigma2_e = pow(sigma2,e,N)
+sigma_mu_e = pow(sigma_mu,e,N)
+assert(sigma1_e*sigma2_e//sigma_mu_e == mu*m1*m2)
+#if the above is true, can we not derive the following?
+sigma = (sigma1*sigma2//sigma_mu)%N      
+PrintSomethingOutHex("sigma",sigma)   
 PrintSomethingOutDec("sigmaok/sigma",Decimal(sigmaok/sigma))
 #
+if Verify(m,sigma):
+   print("Well done!")
+else:     
+   print("Try Again!")
+#
 Oracle_Disconnect()
-print("\nFinished")
+print("Finished")
